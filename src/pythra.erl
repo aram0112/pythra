59d9083fd7be9fbec725957ab2bbd89ef423fd16
aram0112 <48899851+aram0112@users.noreply.github.com>
2023-04-11 19:10:02 -0500

    Update pythra.erl

--- src/pythra.erl
+++ src/pythra.erl
@@ -30,7 +30,8 @@ start_link() ->
 start_link(Paths = [Path | _], Extra) when is_list(Path) ->
     PythraPath = filename:join(code:priv_dir(pythra), "python"),
     PPaths = lists:append(Paths, [PythraPath]),
-    Opts = lists:merge([{python_path, PPaths}, {python, ?PYTHON_VERSION}], Extra),
+    PVersion = os.getenv("ERLPORT_PYTHON", ?PYTHON_VERSION),
+    Opts = lists:merge([{python_path, PPaths}, {python, PVersion}], Extra),
     {ok, Py} = python:start_link(Opts),
     on_start(Py),
     {ok, Py};
